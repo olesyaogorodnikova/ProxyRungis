f802ac1e6a3d9a01b2129aca03052fc251d39505
gauthier HUBERT <gauthier.hubert@ehl.ch>
2019-08-27 17:24:31 +0200

    update

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,14 +1,6 @@
 class OrdersController < ApplicationController
   def index
     @orders = Order.all
-   # @orders_geocoded = Order.geocoded
-
-   # @markers = @orders_geocoded.map do |order|
-    #  {
-     #   lat: order.latitude,
-      #  lng: order.longitude
-      #}
-    #end
   end
 
   def create
@@ -21,28 +13,7 @@ class OrdersController < ApplicationController
     end
   end
 
-
   def show
     @order = current_user.orders.find(params[:id])
   end
-
-  def edit
-    @order = Order.find(params[:id])
-    if @order.save
-      redirect_to orders_path
-    else
-      render :edit
-    end
-  end
-
-  def create
-
-  end
-
-  def update
-    @order = Order.find(params[:id])
-    @order.update(params[:order])
-    redirect_to orders_path
-  end
-
 end
