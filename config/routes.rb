c344303f769287af2952f42b0481878d4856dd3d
Christophe Bensmaine <christophe.bensmaine@laposte.net>
2019-08-28 11:10:49 +0200

    routes for modal

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 
-  resources :packages
+  resources :packages do
+    resources :cart_items, only: :create
+  end
   resources :orders, only: [:show] do
     resources :payments, only: [:new, :create]
 
