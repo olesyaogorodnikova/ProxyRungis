c8755d02be48dbb08980573da4fecbf56fd829f6
Christophe Bensmaine <christophe.bensmaine@laposte.net>
2019-08-28 13:29:38 +0200

    cart-table done

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -11,9 +11,9 @@ class CartItemsController < ApplicationController
 
     @cart_item = CartItem.new(cart_item_params)
     @cart_item.package = @package
-    @cart_item.cart = current_user.cart
+    @cart_item.cart = current_user.cart || Cart.new(user: current_user)
 
-    if @cart_item.save!
+    if @cart_item.save
       redirect_to packages_path
     else
       render 'packages/show'
---
+++ app/controllers/carts_controller.rb
@@ -0,0 +1,46 @@
+class CartsController < ApplicationController
+
+
+  def new
+    @user = current_user
+    @cart = Cart.new
+  end
+
+  def show
+    @user = current_user
+    @cart = current_user.cart
+    @cart_items = current_user.cart.cart_items
+    #params[:cart][:cart_items_id].reject(&:blank?).each do |cart|
+    #   @cart = Cart.create(user: user_id, cart_items: @cart_items)
+    #   flash.keep[:error] = "#{@cart.errors.full_messages.join(' ')}" unless @cart.valid?
+    # end
+    # redirect_to packages_path
+  end
+
+ private
+
+  def cart_params
+    params.require(:cart).permit(:user_id)
+  end
+end
+
+  # def new
+  #   @plant = Plant.find(params[:plant_id])
+  #   @plant_tag = PlantTag.new
+  # end
+
+#   def create
+#     @plant = Plant.find(params[:plant_id])
+#     params[:plant_tag][:tag_id].reject(&:blank?).each do |tag_id|
+#       @plant_tag = PlantTag.create(tag_id: tag_id, plant: @plant)
+#       flash.keep[:error] = "#{@plant_tag.errors.full_messages.join(' ')}" unless @plant_tag.valid?
+#     end
+#     redirect_to garden_path(@plant.garden)
+#   end
+
+#   private
+
+#   def plant_tag_params
+#     params.require(:plant_tag).permit(:tag_id)
+#   end
+# end
