367be630432f32d95839c9c6bee3b2cee54848a1
quentin deleglise <deleglise.quentin@hotmail.fr>
2019-08-29 15:44:37 +0200

    Ajout de la map et center de la map + form + la view edit de order

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_28_135307) do
+ActiveRecord::Schema.define(version: 2019_08_29_123904) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,6 +69,8 @@ ActiveRecord::Schema.define(version: 2019_08_28_135307) do
     t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
   end
 
   create_table "users", force: :cascade do |t|
