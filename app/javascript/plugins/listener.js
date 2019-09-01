d8e6bb9aa3876e2afe3771db8a89d50cd5da3a93
Christophe Bensmaine <christophe.bensmaine@laposte.net>
2019-09-01 14:34:06 +0200

    new display of cart as a modal

--- app/javascript/plugins/listener.js
+++ app/javascript/plugins/listener.js
@@ -1,19 +1,12 @@
-const cible = document.querySelector('#icon-button-shopping-basket');
-const button = document.querySelector('#button-validation-cart-item');
-
-button.addEventListener('click', (e) => {
-  e.target.classList.add('active');
-  e.target.innerText = 'Votre commande en cours';
-});
-
-const basketWithCartItems = (cart) => {
-  return cible.target.classList.add('active')
-};
 
 
 
 
-function activateCart(newColor) {
-  var elem = document.getElementById('button-validation-cart-item');
-  elem.style.color = newColor;
-}
+
+
+
+
+
+
+
+
