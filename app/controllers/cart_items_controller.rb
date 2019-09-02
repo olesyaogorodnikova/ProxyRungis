0a3dfba27d2843add54c14d65bc08f4ec5e31779
Christophe Bensmaine <christophe.bensmaine@laposte.net>
2019-09-02 12:49:32 +0200

    new display for cart, for showing packages

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -27,7 +27,10 @@ end
   def destroy
     @cart_item = CartItem.find(params[:id])
     @cart_item.destroy
-    redirect_to packages_path
+    respond_to do |format|
+      format.html { redirect_to packages_path }
+      format.js
+    end
   end
 
  private
