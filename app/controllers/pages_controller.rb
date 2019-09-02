4a9efa5f6ff59a8e37423dc6678312db71e3f6c9
gauthier HUBERT <gauthier.hubert@ehl.ch>
2019-09-02 14:22:58 +0200

    update dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,13 @@
 # app/controllers/pages_controller.rb
 class PagesController < ApplicationController
-  # skip_before_action :authenticate_user!, only: :home
+  skip_before_action :authenticate_user!, only: :home
 
   def home
     Cart.create(user: current_user) if current_user.cart.nil?
+  end
+
+  def dashboard
+    @orders = Order.all
+    @myorders = current_user.orders
   end
 end
