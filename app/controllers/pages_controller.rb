97af6d837e784d9b304f88c900d276cbd0b60343
gauthier HUBERT <gauthier.hubert@ehl.ch>
2019-09-02 17:51:42 +0200

    dashboard dynamic

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 # app/controllers/pages_controller.rb
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: :home
+  skip_before_action :authenticate_user!, only: [:home, :dashboard]
 
   def home
     Cart.create(user: current_user) if current_user.cart.nil?
