50efa6672539993a0290c5e43162f1b9e26ddb37
quentin deleglise <deleglise.quentin@hotmail.fr>
2019-09-06 11:19:49 +0200

    je retire un fichier qui fait sans doute bug le push heroku

--- config/initializers/algoliasearch.rb
+++
@@ -1,6 +0,0 @@
-AlgoliaSearch.configuration = {
- application_id: '6I1I42E5YC',
- api_key: ENV['ALGOLIA_SECRET_KEY']
-}
-
-AlgoliaSearch.configuration = { application_id: '6I1I42E5YC', api_key: 'ENV['ALGOLIA_SECRET_KEY']' }
